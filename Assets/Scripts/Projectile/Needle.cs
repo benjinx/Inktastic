64bfc26be65140bab38000461ef468dafbd829da
Ben <mynameizkevin@gmail.com>
2025-01-22 12:21:45 -0500

    Added projectile png

--- Assets/Scripts/Projectile/Needle.cs
+++ Assets/Scripts/Projectile/Needle.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
-//[RequireComponent(typeof(Rigidbody))]
 public class Needle : MonoBehaviour
 {
-    public Transform target;
+    public Transform floatTarget;
 
     public float floatRadius = 0.25f;
     public float speed = 0.5f;
 
     private float seedX, seedY, seedZ;
+
+    private bool isFloating = true;
+
+    private bool isShooting = false;
+
+    private Transform hitTarget;
 
     void Start()
     {
@@ -19,12 +24,54 @@ public class Needle : MonoBehaviour
 
     void Update()
     {
-        float x = Mathf.PerlinNoise(seedX, Time.time * speed) * 2.0f - 1.0f;
-        float y = Mathf.PerlinNoise(seedY, Time.time * speed) * 2.0f - 1.0f;
-        float z = Mathf.PerlinNoise(seedZ, Time.time * speed) * 2.0f - 1.0f;
+        if (isFloating)
+        {
+            float x = Mathf.PerlinNoise(seedX, Time.time * speed) * 2.0f - 1.0f;
+            float y = Mathf.PerlinNoise(seedY, Time.time * speed) * 2.0f - 1.0f;
+            float z = Mathf.PerlinNoise(seedZ, Time.time * speed) * 2.0f - 1.0f;
 
-        Vector3 offset = new Vector3(x, y, z) * floatRadius;
+            Vector3 offset = new Vector3(x, y, z) * floatRadius;
 
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, speed);
+            transform.position = Vector3.Lerp(transform.position, floatTarget.position + offset, speed);
+        }
+
+        if (isShooting)
+        {
+            transform.position = Vector3.Lerp(transform.position, hitTarget.position, speed * Time.deltaTime);
+        }
+    }
+
+    // Called to shoot the spike
+    public void ShootSpike(Transform target)
+    {
+        isFloating = false;
+
+        isShooting = true;
+
+        hitTarget = target;
+    }
+
+    // Called when we hit the target
+    public void TargetHit()
+    {
+        transform.parent = hitTarget;
+        isShooting = false;
+
+        // Despawn after 5 seconds
+        Invoke(nameof(Despawn), 5);
+    }
+
+    // Despawn/disable spike
+    private void Despawn()
+    {
+        enabled = false;
+    }
+
+    // Target has been asked to be reset above character
+    public void ResetSpike()
+    {
+        transform.position = floatTarget.position; // Move back to our anchor
+        isFloating = true;
+        enabled = true;
     }
 }
