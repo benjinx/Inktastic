d5648939857a0bb0e7de26870529fea3bbc70810
BizarreFog <bizarrefog@gmail.com>
2025-01-24 01:31:17 -0600

    Placeholder enemy death, imports test environment

--- Assets/Scripts/Combat/CombatHandler.cs
+++ Assets/Scripts/Combat/CombatHandler.cs
@@ -70,6 +70,11 @@ public class CombatHandler : GameplayBehaviour
             PlayerStateMachine psm = GetComponent<PlayerStateMachine>();
             psm.ChangeState(psm.deathState);
         }
+
+        if(GetComponent<EnemyStateMachine>() != null)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void InitializeHitboxes()
